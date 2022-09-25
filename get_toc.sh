0111dba063e74ab5d987594a23c2d8f2c6b1aab8
Alex Popov <alexpopov366@gmail.com>
2022-09-25 17:48:59 +0300

    New

--- get_toc.sh
+++ get_toc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+HREF='https:\/\/alexbmstu.github.io\/2022\/main_frame.html'
 cat 10*.md > 1.txt
 #cat 20*.md >> 1.txt
 #cat 30*.md >> 1.txt
@@ -11,6 +11,7 @@ cat 10*.md > 1.txt
 #cat 9*.md >> 1.txt
 #cat appendix.md >> 1.txt
 cat 1.txt | grep \^\#  > toc.md
+sed -i "s/<a name=\"/](<a name=$HREF@/g" toc.md
 sed -i 's/# /#/g' toc.md
 sed -i 's/####/\t\t\t- [/g' toc.md
 sed -i 's/###/\t\t- [/g' toc.md
@@ -19,4 +20,5 @@ sed -i 's/#/- [/g' toc.md
 sed -i 's/ <a name="/](#/g' toc.md
 sed -i 's/<a name="/](#/g' toc.md
 sed -i 's/"><\/a>/)/g' toc.md
+sed -i 's/@/#/g' toc.md
 rm 1.txt
